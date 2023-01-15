f6dcd4c5e18d0c6f487e2bc62513d65fbcb05ae3
Gregorio Santos <gregoriohd@gmail.com>
2023-01-15 19:06:34 -0300

    commit inicial

---
+++ src/main/java/br/com/gregoriohd/BackendAttornatusApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.gregoriohd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendAttornatusApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendAttornatusApiApplication.class, args);
+	}
+
+}
